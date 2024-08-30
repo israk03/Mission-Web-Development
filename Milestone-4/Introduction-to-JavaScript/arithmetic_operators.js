774f6eefff7d6302da711a12a0e73286f21df301
Mohammad Israk Chowdhury <israkchowdhury03@gmail.com>
2024-08-30 07:32:39 +0600

    basic of js

---
+++ Milestone-4/Introduction-to-JavaScript/arithmetic_operators.js
@@ -0,0 +1,23 @@
+let a = 10;
+let b = 3;
+
+let addition = a + b;
+console.log(addition);
+
+let subtraction = a - b;
+console.log(subtraction);
+
+let multi = a * b;
+console.log(multi);
+
+let division = a / b;
+console.log(division);
+
+let modulus = a % b;
+console.log(modulus);
+
+a++; 
+// now a is 11
+
+b--;
+// now b is 2
---
+++ Milestone-4/Introduction-to-JavaScript/data_type.js
@@ -0,0 +1,24 @@
+    // Numeric
+
+// integer
+let age = 25;
+console.log(age);
+
+// float
+let price = 59.45;
+
+    // String
+
+// single quotes
+let firstName = 'Israk';
+
+// double quotes
+let lastName = "Chowdhury";
+
+    // Boolean 
+
+// true
+let isSingle = true;
+
+// false
+let isRich = false;

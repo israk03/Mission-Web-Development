7dfa4cab1e02fff340a97af1b360a678d91804dc
Mohammad Israk Chowdhury <israkchowdhury03@gmail.com>
2024-09-27 01:30:07 +0600

    string

---
+++ Milestone-6/JS-Recap-and-Basic-ES6/string.js
@@ -0,0 +1,18 @@
+// template string
+let name = "Israk";
+let greeting = `Hello, ${name}.`;
+console.log(greeting);
+
+
+// multiple line string
+let multiple = `This is line 1
+This is line 2
+This is line 3`;
+console.log(multiple);
+
+
+// dynamic string
+let namee = "Samiha";
+let age = 16;
+let intro = `My name is ${namee} and I am ${age} years old.`;
+console.log(intro);

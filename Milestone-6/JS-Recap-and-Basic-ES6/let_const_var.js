7041bf609fa56c0d8ce747ee2f70f0b07685fd5d
Mohammad Israk Chowdhury <israkchowdhury03@gmail.com>
2024-09-27 01:11:42 +0600

    var-let-const

---
+++ Milestone-6/JS-Recap-and-Basic-ES6/let_const_var.js
@@ -0,0 +1,11 @@
+// var => can reassign and redeclare
+var x = 5; 
+console.log(x);
+
+// let => can't redeclare but reassign
+let y = 10;
+console.log(y);
+
+// const => can't reassign 
+const z = 15;
+console.log(15);

7e5593eebb0537f9fda78dbc907e8ccbc4311c24
Mohammad Israk Chowdhury <israkchowdhury03@gmail.com>
2024-10-21 20:26:13 +0600

    react useState

---
+++ Milestone-7-Explore-react/src/App.jsx
@@ -0,0 +1,33 @@
+import './App.css'
+import Counter from './counter';
+
+function App() {
+
+  function handleClick(){
+    alert('Button 1 clicked.');
+  }
+
+  const handleClick2 = () => {
+    alert('Button 2 clicked.');
+  }
+
+  const addToFive = (num) => {
+    alert(num + 5);
+  }
+
+  return (
+    <>
+      <h2>React Core Concepts</h2>
+
+      <Counter></Counter>
+
+      {/* Event handler */}
+      <button onClick={handleClick}>CLICK 1</button>
+      <button onClick={handleClick2}>CLICK 2</button>
+      <button onClick={() => {alert('Button 3 clicked.')}}>CLICK 3</button>
+      <button onClick={() => addToFive(5)}>CLICK 4</button>
+    </>
+  )
+}
+
+export default App

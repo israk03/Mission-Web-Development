27ab891ac51c2d763e2b6fe7ed8af8d0536a9cce
Mohammad Israk Chowdhury <israkchowdhury03@gmail.com>
2025-01-26 23:25:34 +0600

    Create React form and Post API and send data to the server

--- Getting-Started-with-Node-Express-and-Api/user-management-client/src/App.jsx
+++ Getting-Started-with-Node-Express-and-Api/user-management-client/src/App.jsx
@@ -9,10 +9,54 @@ function App() {
       .then((res) => res.json())
       .then((data) => setUsers(data));
   }, []);
+
+  const handleAddUser = (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+
+    const user = { name, email };
+    console.log(user);
+
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("Inside post response: ", data);
+
+        const newUsers = [...users, data];
+        setUsers(newUsers);
+
+        form.reset();
+      });
+  };
+
   return (
     <>
       <h1>User Management Client</h1>
       <h3>Number of users: {users.length}</h3>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" id="" />
+        <br />
+        <input type="email" name="email" id="" />
+        <br />
+        <input type="submit" value="Add user" />
+      </form>
+      <div>
+        {users.map((user) => (
+          <p key={user.id}>
+            {user.id} - {user.name} : {user.email}
+          </p>
+        ))}
+      </div>
     </>
   );
 }
